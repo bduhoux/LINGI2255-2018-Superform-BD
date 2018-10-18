6926bccbf45d68733a211e69b92b3121ce17f8a7
guiste10 <guillaume.neirinckx@student.uclouvain.be>
2018-10-18 16:19:15 +0200

    ajouter comms mail.py

--- superform/superform/plugins/mail.py
+++ superform/superform/plugins/mail.py
@@ -5,21 +5,21 @@ from smtplib import SMTPException
 from flask import current_app
 import json
 
-FIELDS_UNAVAILABLE = ['Title','Description']
+FIELDS_UNAVAILABLE = ['Title','Description'] #list of field names that are not used by your module
 
-CONFIG_FIELDS = ["sender","receiver"]
+CONFIG_FIELDS = ["sender","receiver"] #This lets the manager of your module enter data that are used to communicate with other services.
 
-def run(publishing,channel_config):
-    json_data = json.loads(channel_config)
-    sender = json_data['sender']
-    receivers = json_data['receiver']
-    msg = MIMEMultipart()
+def run(publishing,channel_config): #publishing:DB channelconfig:DB channel
+    json_data = json.loads(channel_config) # to a Python object
+    sender = json_data['sender'] # data sur le sender ds channelconfig(= dictionnaire)
+    receivers = json_data['receiver'] # data sur le receiver ds channelconfig(= dictionnaire)
+    msg = MIMEMultipart() # # Create the container email message
     msg['From'] = sender
     msg['To'] = receivers
     msg['Subject'] = publishing.title
 
     body = publishing.description
-    msg.attach(MIMEText(body, 'plain'))
+    msg.attach(MIMEText(body, 'plain')) # ajouter contenu texte publication
 
     try:
         smtpObj = smtplib.SMTP(current_app.config["SMTP_HOST"],current_app.config["SMTP_PORT"])
