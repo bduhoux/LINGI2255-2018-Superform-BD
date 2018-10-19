940600a4e7068b9a75a582d2551fc918c7a2121f
bastinjul <julien.bastin@student.uclouvain.be>
2018-10-19 14:33:37 +0200

    Tests unitaires ok et modification plugin facebook -> ajout de fonctions

--- superform/superform/plugins/facebook.py
+++ superform/superform/plugins/facebook.py
@@ -2,33 +2,55 @@ from flask import current_app
 import json
 import facebook
 
-FIELDS_UNAVAILABLE = ['Title','Description'] #list of field names that are not used by your module
+FIELDS_UNAVAILABLE = ['Title', 'Description']  # list of field names that are not used by your module
 
-CONFIG_FIELDS = ["page_id","access_token"] #This lets the manager of your module enter data that are used to communicate with other services.
+CONFIG_FIELDS = ["page_id",
+                 "access_token"]  # This lets the manager of your module enter data that are used to communicate with other services.
 
 
-def run(publishing,channel_config): #publishing:DB channelconfig:DB channel
-    json_data = json.loads(channel_config)  # to a Python object
-    page_id = json_data['page_id']  # data sur le sender ds channelconfig(= dictionnaire)
+def run(publishing, channel_config):  # publishing:DB channelconfig:DB channel
+    page_id = get_page_id(channel_config)  # data sur le sender ds channelconfig(= dictionnaire)
+    access_token = get_access_token(channel_config)  # data sur le receiver ds channelconfig(= dictionnaire)
 
-    access_token = json_data['access_token']  # data sur le receiver ds channelconfig(= dictionnaire)
-    cfg = {
-        "page_id": page_id,  # Step 1
-        "access_token": access_token  # Step 3
-    }
+    cfg = get_config(page_id, access_token)
     api = get_api(cfg)
 
-
-    msg = publishing.title + "\n\n" + publishing.description
-    link = publishing.link_url
+    msg = get_message(publishing)
+    link = get_link(publishing)
     status = api.put_object(
         parent_object="me",
         connection_name="feed",
         message=msg,
-        link = link
+        link=link
     )
 
 
 def get_api(cfg):
     graph = facebook.GraphAPI(cfg['access_token'])
     return graph
+
+
+def get_page_id(config):
+    json_data = json.loads(config)
+    return json_data['page_id']
+
+
+def get_access_token(config):
+    json_data = json.loads(config)
+    return json_data['access_token']
+
+
+def get_config(page_id, access_token):
+    cfg = {
+        "page_id": page_id,  # Step 1
+        "access_token": access_token  # Step 3
+    }
+    return cfg
+
+
+def get_message(publishing):
+    return publishing.title + "\n\n" + publishing.description
+
+
+def get_link(publishing):
+    return publishing.link_url
--- superform/superform/tests/test_facebook.py
+++ superform/superform/tests/test_facebook.py
@@ -3,8 +3,10 @@ import tempfile
 
 import pytest
 
-from superform import app,db
+from superform import app, db
+from superform.models import Publishing, Channel
 from superform.plugins import facebook
+
 
 @pytest.fixture
 def client():
@@ -21,6 +23,21 @@ def client():
     os.close(db_fd)
     os.unlink(app.config['DATABASE'])
 
-def test_run_facebook(client):
 
-    assert True == True
+def test_run_facebook(client):
+    new_channel = Channel(config={"page_id": "453122048545115",
+                                  "access_token": "EAAEg6h9DvQwBADFqgmWtCnqXKXTFZAxxxIUHH3iZBHyj54pTYdykE39JQHhM9yOgFOigGCd3FZCqZCZA6wsmPUX7fGyea4Mso6aZBSvmGUZAjWKLzTSZCLZBHVfUtDRYKKNqcZA4Qe5JLzeaZC2THwERHrsTEZBHfHueHZBETYxBHCrZAmiajhFlki3SLZA"})
+
+    assert facebook.get_page_id(new_channel.config) == "453122048545115"
+
+    assert facebook.get_access_token(
+        new_channel.config) == "EAAEg6h9DvQwBADFqgmWtCnqXKXTFZAxxxIUHH3iZBHyj54pTYdykE39JQHhM9yOgFOigGCd3FZCqZCZA6wsmPUX7fGyea4Mso6aZBSvmGUZAjWKLzTSZCLZBHVfUtDRYKKNqcZA4Qe5JLzeaZC2THwERHrsTEZBHfHueHZBETYxBHCrZAmiajhFlki3SLZA"
+
+    assert facebook.get_config(facebook.get_page_id(new_channel.config), facebook.get_access_token(new_channel.config)) == {"page_id": "453122048545115",
+                                  "access_token": "EAAEg6h9DvQwBADFqgmWtCnqXKXTFZAxxxIUHH3iZBHyj54pTYdykE39JQHhM9yOgFOigGCd3FZCqZCZA6wsmPUX7fGyea4Mso6aZBSvmGUZAjWKLzTSZCLZBHVfUtDRYKKNqcZA4Qe5JLzeaZC2THwERHrsTEZBHfHueHZBETYxBHCrZAmiajhFlki3SLZA"}
+
+    new_publish = Publishing(title="Coucou", description="J'aime faire des tests unitaires", link_url="https://docs.pytest.org/en/latest/index.html")
+
+    assert facebook.get_message(new_publish) == "Coucou\n\nJ'aime faire des tests unitaires"
+
+    assert facebook.get_link(new_publish) == "https://docs.pytest.org/en/latest/index.html"
