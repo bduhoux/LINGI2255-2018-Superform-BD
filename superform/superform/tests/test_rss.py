d92570a12a9dbe52a58c2471f92b31f1849db578
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2018-10-23 14:22:28 +0200

    feat(test) : add test_channel_rss

--- superform/superform/tests/test_rss.py
+++ superform/superform/tests/test_rss.py
@@ -1,11 +1,35 @@
 from superform.utils import datetime_converter, str_converter, get_module_full_name
 from superform import app, db, Post, User
 from superform.models import Authorization, Channel
-from superform.users import  is_moderator, get_moderate_channels_for_user,channels_available_for_user
+from superform.users import is_moderator, get_moderate_channels_for_user, channels_available_for_user
 from superform.feed import rss_feed
+from superform.models import db, Publishing, Channel
 
 
 def test_get_module_rss():
     module_name = "rss"
     m = get_module_full_name(module_name)
     assert m == "superform.plugins.rss"
+
+
+def test_channel_rss():
+    """
+    For the sake of this test, you must erase all the channels with rss
+    :return:
+    """
+    channel = Channel.query.filter_by(
+        module='superform.plugins.rss'
+    ).first()
+
+    assert channel is None
+
+    channel = Channel(name="test", module=get_module_full_name("rss"), config="{}")
+    db.session.add(channel)
+
+    channel = Channel.query.filter_by(
+        module='superform.plugins.rss'
+    ).first()
+
+    assert channel is not None
+
+    assert channel.name == 'test'
