dbbcc0a2212f248f43d5eafdd2bb0c15906bcaba
bensim602 <benjamin.simon@student.uclouvain.be>
2018-10-25 23:09:15 +0200

    store publishing  in db with channel id

--- superform/superform/posts.py
+++ superform/superform/posts.py
@@ -39,7 +39,7 @@ def create_a_publishing(post, chn, form):
     if chn.module == "superform.plugins.Twitter":
         extra['truncated'] = form.get(chan + "_truncate") == "Truncate"
 
-    pub = Publishing(post_id=post.id, channel_id=chan, state=0, title=title_post, description=descr_post,
+    pub = Publishing(post_id=post.id, channel_id=chn.id, state=0, title=title_post, description=descr_post,
                      link_url=link_post, image_url=image_post,
                      date_from=date_from, date_until=date_until,
                      extra=json.dumps(extra))
