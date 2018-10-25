b62e69c94c1d7c7ba500fe488d1b01700b44714b
Gui <guillaume.gheysen@student.uclouvain.be>
2018-10-25 13:50:09 +0200

    test get_status
    add 1 test, we have to add more of course

--- superform/superform/tests/test_twitter.py
+++ superform/superform/tests/test_twitter.py
@@ -10,12 +10,15 @@ import unittest
 import plugins.Twitter as Twitter
 from superform import app
 
+cha_conf = json.dumps({"Access token": "1052533183151886336-RBoq1epkAOeRfGdd2pBrbi9uTxQBv6",
+                       "Access token secret": "vqM1nqgcst0uNDSryuMGjhCjT9ldCj4rFUpfxJfDzuTzc"})
+
 
 class Publishing:
     def __init__(self, post_id, title, description, link_url, image_url,
-                 date_from, date_until, option=None, channel_id="Google+ INGI-UCL", state=-1):
+                 date_from, date_until, option=None, channel_id="Twitter Superform Test", state=-1):
         if option is None:
-            option = [{"disablesharing": True, "disablecomments" : True, "circles": "Fanfan_Le_FDP"}]
+            option = {"truncated": False}
         self.post_id = post_id
         self.channel_id = channel_id
         self.state = state
@@ -31,20 +34,21 @@ class Publishing:
 class TestGPlus(unittest.TestCase):
     def test_login(self):
         with app.app_context():
-            cha_conf = json.dumps({"Access token": "1052533183151886336-RBoq1epkAOeRfGdd2pBrbi9uTxQBv6",
-                                    "Access token secret": "vqM1nqgcst0uNDSryuMGjhCjT9ldCj4rFUpfxJfDzuTzc"})
             twit = Twitter.get_api(cha_conf)
             a = twit.GetUser(screen_name="SuperformD")
             self.assertEqual(a.name, "SuperformDev01")
 
+    def test_publishing_1(self):
+        with app.app_context():
+            my_publy = Publishing(0, "Why Google+ is still relevant, even though it will soon cease to exist",
+                                  "And Jesus said : This is my body",
+                                  "www.chretienDeTroie.fr",
+                                  None, " 24-12-2018", "12-12-2222")
+            twit = Twitter.get_api(cha_conf)
+            c = Twitter.getStatus(my_publy, twit)
+            self.assertEqual(c, my_publy.description + " " + my_publy.link_url)
+            self.assertLessEqual(len(c), 280)
 
-
-
-    def test_publishing(self):
-        my_publy = Publishing(0, "Why Google+ is still relevant, even though it will soon cease to exist",
-                              "And Jesus said : This is my body",
-                              "www.chretienDeTroie.fr",
-                              None, " 24-12-2018", "12-12-2222")
 
 
 if __name__ == "__main__":
