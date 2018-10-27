e57c45d9541a2173808fc7c3dd2555b0485b8672
sprt <hellosprt@gmail.com>
2018-10-27 16:13:50 +0200

    Remove hacky code when filtering DateTimes

--- superform/superform/feed.py
+++ superform/superform/feed.py
@@ -1,26 +1,24 @@
-from datetime import datetime
+from datetime import date, datetime
 
 from feedgen.feed import FeedGenerator
 from flask import Blueprint, url_for, request, redirect, render_template, session
 
 from superform.utils import login_required, datetime_converter, str_converter
-from superform.models import db, Publishing, Channel
+from superform.models import db, Publishing
 
 feed_page = Blueprint('feed', __name__)
 
 
 @feed_page.route('/rss.xml', methods=['GET'])
 def rss_feed():
-    now = datetime.now().date()
+    # SQLAlchemy wants datetime objects (not date) when comparing db.DateTime objects,
+    # so we convert today's date to a datetime object.
+    now = datetime(*date.today().timetuple()[:3])
 
     publishings = db.session.query(Publishing).filter(
         Publishing.channel.has(module='superform.plugins.rss'),
         Publishing.state == 1,  # validated/shared
-
-        # XXX: Doesn't work properly when Publishing.date_from == now == Publishing.date_until.
-        # Not sure why. Let's investigate later.
-        # Publishing.date_from <= now,
-
+        Publishing.date_from <= now,
         now <= Publishing.date_until,
     ).all()
 
@@ -30,10 +28,6 @@ def rss_feed():
     feed.description('Superform')
 
     for publishing in publishings:
-        # HACK: only way we've found for now (see the query).
-        if not publishing.date_from.date() <= now:
-            continue
-
         entry = feed.add_entry()
         entry.title(publishing.title)
         entry.description(publishing.description)
