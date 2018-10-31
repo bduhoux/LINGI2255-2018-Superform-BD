51820488ec2f92259f064c86b78248204e831221
bensim602 <benjamin.simon@student.uclouvain.be>
2018-10-31 16:09:39 +0100

    check empty description

--- superform/superform/templates/plugins/twitter.js
+++ superform/superform/templates/plugins/twitter.js
@@ -17,22 +17,33 @@ $('input.checkbox').change(function () {
  */
 function getCharCounter(channelName) {
     function charCounter(event) {
-        var text_length = $('#'+channelName+'_descriptionpost').val().length;
-        if ($('#'+channelName+'_linkurlpost').val() != '' && $('#'+channelName+'_linkurlpost').val() != null)
+        var text_length = $('#' + channelName + '_descriptionpost').val().length;
+        if ($('#' + channelName + '_linkurlpost').val() != '' && $('#' + channelName + '_linkurlpost').val() != null)
             text_length = text_length + 23;
         if (text_length >= 280) {
             $("label[for='" + channelName + "_" + $('#descriptionpost').attr('id') + "'] > a").remove();
             $("label[for='" + channelName + "_" + $('#descriptionpost').attr('id') + "']").append('<a href="#" data-toggle="popover" title="Content too long" data-content="Too many characters for one tweet"><i class="fas fa-exclamation-circle" style="color:orange"></i></a>');
-            $("."+channelName+"_status_too_many_chars").remove();
-            $("#"+channelName+"_card_body").append('<div class="'+channelName+'_status_too_many_chars"> Too many characters for one tweet! </div>');
-            $("#card_body").append('<div class="'+channelName+'_status_too_many_chars">'+channelName+': Too many characters for one tweet! </div>');
+            $("." + channelName + "_status_too_many_chars").remove();
+            $("#" + channelName + "_card_body").append('<div class="' + channelName + '_status_too_many_chars"> Too many characters for one tweet! </div>');
+            $("#card_body").append('<div class="' + channelName + '_status_too_many_chars">' + channelName + ': Too many characters for one tweet! </div>');
             $('[data-toggle="popover"]').popover();
+        }
+        else if (text_length == 0) {
+            $("." + channelName + "_empty_description").remove();
+            $("#" + channelName + "_card_body").append('<div class="' + channelName + '_empty_description"> Twitter publishings must contain a description! </div>');
+            $("#card_body").append('<div class="' + channelName + '_empty_description">' + channelName + ': Twitter publishings must contain a description! </div>');
+            $("#publish-button").prop('disabled', true);
+        }
+        else if (text_length != 0) {
+            $("." + channelName + "_empty_description").remove();
+            $("#publish-button").prop('disabled', false);
         } else {
             $("label[for='" + channelName + "_" + $('#descriptionpost').attr('id') + "'] > a").remove();
-            $("."+channelName+"_status_too_many_chars").remove();
+            $("." + channelName + "_status_too_many_chars").remove();
         }
-        $('#'+channelName+'_textarea_feedback').html(text_length + '/279');
+        $('#' + channelName + '_textarea_feedback').html(text_length + '/279');
     }
+
     return charCounter;
 }
 
@@ -44,13 +55,13 @@ function getCharCounter(channelName) {
  */
 function initializeTwitterListeners(channelName, channelID) {
     getCharCounter(channelName)(null);
-    $('#'+channelName+'_textarea_feedback').html('0/279');
-    $('#'+channelName+'_descriptionpost').on('keyup', getCharCounter(channelName));
-    $('#'+channelName+'_linkurlpost').on('keyup', getCharCounter(channelName));
+    $('#' + channelName + '_textarea_feedback').html('0/279');
+    $('#' + channelName + '_descriptionpost').on('keyup', getCharCounter(channelName));
+    $('#' + channelName + '_linkurlpost').on('keyup', getCharCounter(channelName));
     $('#descriptionpost').on('keyup', getCharCounter(channelName));
     $('#linkurlpost').on('keyup', getCharCounter(channelName));
-    $('#li_'+channelName).on('click', getCharCounter(channelName));
-    $('#chan_option_'+channelID).on('keyup', getCharCounter(channelName));
+    $('#li_' + channelName).on('click', getCharCounter(channelName));
+    $('#chan_option_' + channelID).on('keyup', getCharCounter(channelName));
 }
 
 
@@ -60,25 +71,25 @@ function initializeTwitterListeners(channelName, channelID) {
  * @param channelID: The ID of the channel
  */
 function removeTwitterListeners(channelName, channelID) {
-    $('#'+channelName+'_descriptionpost').off('keyup');
-    $('#'+channelName+'_linkurlpost').off('keyup');
+    $('#' + channelName + '_descriptionpost').off('keyup');
+    $('#' + channelName + '_linkurlpost').off('keyup');
     $('#descriptionpost').off('keyup');
     $('#linkurlpost').off('keyup');
-    $('#li_'+channelName).off('click');
-    $('#chan_option_'+channelID).off('keyup');
-    $("."+channelName+"_status_too_many_chars").remove();
+    $('#li_' + channelName).off('click');
+    $('#chan_option_' + channelID).off('keyup');
+    $("." + channelName + "_status_too_many_chars").remove();
 }
 
 function twitterUpdatePreview(channelName) {
-    var text = $('#'+channelName+'_descriptionpost').val();
-    var url = $('#'+channelName+'_linkurlpost').val();
+    var text = $('#' + channelName + '_descriptionpost').val();
+    var url = $('#' + channelName + '_linkurlpost').val();
     var tweets = splitTweet(text, url);
-    var preview_container = $('#'+channelName+'_preview');
+    var preview_container = $('#' + channelName + '_preview');
     var numberOfTweets = tweets.length;
     console.log(tweets);
     $('.tweet-preview').remove();
     for (var i = 1; i <= numberOfTweets; i++) {
-        var tweet = tweets[i-1];
+        var tweet = tweets[i - 1];
         console.log(tweet);
         var html = `<div class="form-group tweet-preview"><label for="${channelName}_$tweet_{i}">Tweet ${i}/${numberOfTweets}</label><br> <textarea class="form-control" rows="5" id="${channelName}_tweet_${i}" name="${channelName}_tweet_${i}">${tweet}</textarea></div>`;
         preview_container.append(html);
@@ -114,7 +125,7 @@ function splitTweet(text, url) {
     var numberOfTweets = tweet_list.length;
     if (numberOfTweets > 1) {
         for (var i = 1; i <= numberOfTweets; i++) {
-            tweet_list[i-1] = `[${i}/${numberOfTweets}] ` + tweet_list[i-1];
+            tweet_list[i - 1] = `[${i}/${numberOfTweets}] ` + tweet_list[i - 1];
         }
     }
     return tweet_list;
