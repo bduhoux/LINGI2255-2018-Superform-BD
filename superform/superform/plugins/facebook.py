8444783652780af113e331a332d4b17748bb545e
bastinjul <julien.bastin@student.uclouvain.be>
2018-11-02 00:58:33 +0100

    Facebook publication ok

--- superform/superform/plugins/facebook.py
+++ superform/superform/plugins/facebook.py
@@ -9,34 +9,24 @@ CONFIG_FIELDS = ["page_id",
 
 
 def run(publishing, channel_config):  # publishing:DB channelconfig:DB channel
-    """
+
     page_id = get_page_id(channel_config)  # data sur le sender ds channelconfig(= dictionnaire)
     access_token = get_access_token(channel_config)  # data sur le receiver ds channelconfig(= dictionnaire)
-    print(page_id)
-    print(access_token)
 
     cfg = get_config(page_id, access_token)
     api = get_api(cfg)
 
     msg = get_message(publishing)
     link = get_link(publishing)
+    image = get_image(publishing)
+
     status = api.put_object(
         parent_object="me",
         connection_name="feed",
         message=msg,
         link=link
     )
-    """
-    page_access_token = "EAAEg6h9DvQwBAKlNUhF4NGa69OYGlgLJZAZBFDZBbZAk0gFScTX2f0EG0ZAkaiVPEWlRBXePLXgy2d8rnE21GI7Ru4mvZAJ8STI0xIaVN0jOGH5xCE6MIIBr3l9jNQVUSo8XafSdZC6KzomTnMiFGcFsGZB7xaGZChZBpL4VJgpnaC2b8fRQltnmZA91qXXlc6gcdcvZB7DF4ZAeWkhAMuVDY9hlJ"
-    api = facebook.GraphAPI(access_token=page_access_token)
-    msg = "penis"
 
-    api.put_object(
-        parent_object="me",
-        connection_name="feed",
-        message=msg,
-        link=""
-    )
 
 def get_api(cfg):
     graph = facebook.GraphAPI(cfg['access_token'])
@@ -67,3 +57,7 @@ def get_message(publishing):
 
 def get_link(publishing):
     return publishing.link_url
+
+
+def get_image(publishing):
+    return publishing.image_url
