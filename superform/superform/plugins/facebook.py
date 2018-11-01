c6fe63d418b1f057bb06d368e06115b9fc48868f
guiste10 <guillaume.neirinckx@student.uclouvain.be>
2018-11-01 23:29:24 +0100

    ca maaaaarche

--- superform/superform/plugins/facebook.py
+++ superform/superform/plugins/facebook.py
@@ -9,9 +9,9 @@ CONFIG_FIELDS = ["page_id",
 
 
 def run(publishing, channel_config):  # publishing:DB channelconfig:DB channel
+    """
     page_id = get_page_id(channel_config)  # data sur le sender ds channelconfig(= dictionnaire)
     access_token = get_access_token(channel_config)  # data sur le receiver ds channelconfig(= dictionnaire)
-
     print(page_id)
     print(access_token)
 
@@ -26,8 +26,18 @@ def run(publishing, channel_config):  # publishing:DB channelconfig:DB channel
         message=msg,
         link=link
     )
+    """
+    test_user_access_token = "EAAEg6h9DvQwBAKlNUhF4NGa69OYGlgLJZAZBFDZBbZAk0gFScTX2f0EG0ZAkaiVPEWlRBXePLXgy2d8rnE21GI7Ru4mvZAJ8STI0xIaVN0jOGH5xCE6MIIBr3l9jNQVUSo8XafSdZC6KzomTnMiFGcFsGZB7xaGZChZBpL4VJgpnaC2b8fRQltnmZA91qXXlc6gcdcvZB7DF4ZAeWkhAMuVDY9hlJ"
+    api = facebook.GraphAPI(access_token=test_user_access_token)
+    page_id_test = 453122048545115
+    msg = "penis"
 
-
+    api.put_object(
+        parent_object="me",
+        connection_name="feed",
+        message=msg,
+        link=""
+    )
 
 def get_api(cfg):
     graph = facebook.GraphAPI(cfg['access_token'])
