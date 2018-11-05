3c0cf0e68a0bc09391bb9eb49331f864ec425c96
ElNiak <christophe.crochet@student.uclouvain.be>
2018-11-05 17:02:03 +0100

    - V1 Wiki Test

---
+++ superform/superform/tests/test_wiki.py
@@ -0,0 +1,25 @@
+import os
+import tempfile
+import json
+import pytest
+
+
+from superform import app, db
+from superform.models import Publishing, Channel
+from superform.plugins import wiki
+
+@pytest.fixture
+def client():
+    app.app_context().push()
+    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    with app.app_context():
+        db.create_all()
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(app.config['DATABASE'])
+
