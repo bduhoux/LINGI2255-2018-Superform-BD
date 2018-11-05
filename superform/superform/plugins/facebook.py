3530d36f8d21501f80309a569afd23ab0eca17e0
guiste10 <guillaume.neirinckx@student.uclouvain.be>
2018-11-05 13:52:53 +0100

    essai fixer erreurs keys

--- superform/superform/plugins/facebook.py
+++ superform/superform/plugins/facebook.py
@@ -9,13 +9,36 @@ CONFIG_FIELDS = ["page_id",
 
 
 def run(publishing, channel_config):  # publishing:DB channelconfig:DB channel
+    """
+    page_access_token = "EAAEg6h9DvQwBAKlNUhF4NGa69OYGlgLJZAZBFDZBbZAk0gFScTX2f0EG0ZAkaiVPEWlRBXePLXgy2d8rnE21GI7Ru4mvZAJ8STI0xIaVN0jOGH5xCE6MIIBr3l9jNQVUSo8XafSdZC6KzomTnMiFGcFsGZB7xaGZChZBpL4VJgpnaC2b8fRQltnmZA91qXXlc6gcdcvZB7DF4ZAeWkhAMuVDY9hlJ"
+    api = facebook.GraphAPI(access_token=page_access_token)
+    msg = "penis"
 
-    page_id = get_page_id(channel_config)  # data sur le sender ds channelconfig(= dictionnaire)
-    access_token = get_app_id(channel_config)  # data sur le receiver ds channelconfig(= dictionnaire)
+    api.put_object(
+        parent_object="me",
+        connection_name="feed",
+        message=msg,
+        link=""
+    )
+    """
 
-    cfg = get_config(page_id, access_token)
-    api = get_api(cfg)
+    page_id = get_page_id(channel_config)
+    app_id = get_app_id(channel_config)
 
+    cfg = get_config(page_id, app_id)
+
+    # login le user todo
+
+    # publie sur page possedée par cet user
+    #api1 = facebook.GraphAPI(access_token="trouvée au dessus")
+    api1 = facebook.GraphAPI(access_token="EAAEg6h9DvQwBALKixA48eCqk6G7baxSzZBkBVcdifTra4FqhJlasQfxKzKrVuZCLEZCSjHGLhoMukcbmHTcOZCwTZBXLzUBeEdDYctuDsAPN9svmPIsTmJlnPE6akX5dPzEJLHPg7N6ZAZCofARAuiPIZAt2zea0SFI5XZBhegA6boAogk9YZBzFXKrk4zbzV3GwzvqZA9RqqoCaWoHR5o7qG74xsRiTEXaXj0ZD")
+    response = api1.request('/me/accounts?type=page')  # recoit liste des pages gérée par le user et leurs page tokens
+    pages = json.loads(response)
+    for page in pages['data']:
+        print(page["access_token"])
+
+    api = -1
+"""
     msg = get_message(publishing)
     link = get_link(publishing)
     image = get_image(publishing)
@@ -26,12 +49,16 @@ def run(publishing, channel_config):  # publishing:DB channelconfig:DB channel
         message=msg,
         link=link
     )
+"""
 
 
+
+
+"""
 def get_api(cfg):
     graph = facebook.GraphAPI(cfg['access_token'])
     return graph
-
+"""
 
 def get_page_id(config):
     json_data = json.loads(config)
@@ -43,10 +70,10 @@ def get_app_id(config):
     return json_data['app_id']
 
 
-def get_config(page_id, access_token):
+def get_config(page_id, app_id):
     cfg = {
         "page_id": page_id,  # Step 1
-        "access_token": access_token  # Step 3
+        "app_id": app_id  # Step 3
     }
     return cfg
 
