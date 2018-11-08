f07d463899c809cc4ebdb508facfcf204085d679
Shawcker <antoine.popeler@student.uclouvain.be>
2018-11-08 17:56:11 +0100

    tests OK

--- superform/superform/tests/test_delete.py
+++ superform/superform/tests/test_delete.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 import pytest
 
-from superform import app, db, delete
+from superform import app, db
 from superform.models import Post
 from superform.utils import datetime_converter
 
@@ -22,8 +22,25 @@ def client():
     os.unlink(app.config['DATABASE'])
 
 
-def test_delete_publish():
-    user_id = 1
+def login(client, login):
+    with client as c:
+        with c.session_transaction() as sess:
+            if login is not "myself":
+                sess["admin"] = True
+            else:
+                sess["admin"] = False
+
+            sess["logged_in"] = True
+            sess["first_name"] = "gen_login"
+            sess["name"] = "myname_gen"
+            sess["email"] = "hello@genemail.com"
+            sess['user_id'] = login
+
+
+def test_delete(client):
+    user_id = "myself"
+    login(client, user_id)
+
     title_post = "title_test"
     descr_post = "description"
     link_post = "link"
@@ -33,9 +50,14 @@ def test_delete_publish():
     p = Post(user_id=user_id, title=title_post, description=descr_post, link_url=link_post, image_url=image_post,
              date_from=date_from, date_until=date_until)
     db.session.add(p)
+    db.session.commit()
 
     id_post = p.id
 
-    delete.delete_post(id_post)
-    deleted_post = db.session.query(Post).get(id_post)
+    path = '/delete/' + str(id_post)
+
+    client.get(path)
+
+    deleted_post = db.session.query(Post).filter_by(id=id_post).first()
+
     assert deleted_post is None
