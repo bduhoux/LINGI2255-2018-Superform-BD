d21af49c2bc7b5a28e9eda46b8e6fb581aede899
sluyters <arthur.sluyters@student.uclouvain.be>
2018-11-08 15:00:07 +0100

    Test fuunn

---
+++ superform/superform/tests/test_search.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_sql_injections():
+    """
+    Check if the search module prevents SQL injections:
+        - Get publications from all users instead of only this user's
+    :return:
+    """
+
+    pass
+
+def test_basic_search():
+    pass
+
+def test_advanced_search():
+    pass
