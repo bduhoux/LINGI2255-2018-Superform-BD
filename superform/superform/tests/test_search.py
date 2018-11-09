68d8c8d3094b40f8a44397cf97202d3f3573faaf
sluyters <arthur.sluyters@student.uclouvain.be>
2018-11-09 11:19:01 +0100

    test great yeah

--- superform/superform/tests/test_search.py
+++ superform/superform/tests/test_search.py
@@ -1,4 +1,33 @@
 import pytest
+import os
+import tempfile
+from superform import app, db
+
+
+@pytest.fixture
+def client():
+    app.app_context().push()
+    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    with app.app_context():
+        db.create_all()
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(app.config['DATABASE'])
+
+def populate_db():
+    pass
+
+
+def test_basic_search():
+    pass
+
+def test_advanced_search():
+    pass
 
 def test_sql_injections():
     """
@@ -6,11 +35,4 @@ def test_sql_injections():
         - Get publications from all users instead of only this user's
     :return:
     """
-
-    pass
-
-def test_basic_search():
-    pass
-
-def test_advanced_search():
     pass
