2606e3b50108763569438f2b883bbc875ec927ae
TGLuis <brutusltg@gmail.com>
2018-11-20 16:29:44 +0100

    End of Module Search Team 1

--- superform/setup.py
+++ superform/setup.py
@@ -6,26 +6,10 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 24d8ff7... End of Module Search Team 1
         'python3-saml',
         'sqlalchemy',
         'flask_sqlalchemy',
         'feedgen',
         'python-twitter'
     ]
-
-
-<<<<<<< HEAD
-=======
-        'python3-saml', 'sqlalchemy',
-        'flask-sqlalchemy',
-        'python-twitter',
-        'feedgen'
-    ],
->>>>>>> 6d07995... Implement the RSS channel
-=======
->>>>>>> 24d8ff7... End of Module Search Team 1
 )
