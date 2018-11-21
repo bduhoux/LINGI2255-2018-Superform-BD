135bd15d770e2487cc375bd0a53e98a45f8746bb
bensim602 <benjamin.simon@student.uclouvain.be>
2018-11-21 13:19:50 +0100

    merge conflict

--- superform/setup.py
+++ superform/setup.py
@@ -7,26 +7,10 @@ setup(
     install_requires=[
         'apscheduler',
         'flask',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 24d8ff7... End of Module Search Team 1
         'python3-saml',
         'sqlalchemy',
         'flask_sqlalchemy',
         'feedgen',
         'python-twitter'
     ]
-
-
-<<<<<<< HEAD
-=======
-        'python3-saml', 'sqlalchemy',
-        'flask-sqlalchemy',
-        'python-twitter',
-        'feedgen'
-    ],
->>>>>>> 6d07995... Implement the RSS channel
-=======
->>>>>>> 24d8ff7... End of Module Search Team 1
 )
