f2366385fbd5ce9b698a6c880d156b9a221b3852
Sami <sami.bosch@gmail.com>
2018-11-22 16:48:49 +0100

    Send test message to all channels

--- superform/superform/plugins/slack.py
+++ superform/superform/plugins/slack.py
@@ -1,14 +1,33 @@
 import json
-import slackclient
+from slackclient import SlackClient
 
 FIELDS_UNAVAILABLE = []
 # list of field names that are not used by your module
 
 CONFIG_FIELDS = ['token']
+
+
 # This lets the manager of your module enter data that are used to communicate with other services.
 
 
 # appelé dans publishings.py
 def run(publishing, channel_config):
-   json_data = json.loads(channel_config)
-   token = json_data['token']
+    json_data = json.loads(channel_config)
+    token = json_data['token']
+    slack_client = SlackClient(token)
+
+    channels = slack_client.api_call(
+        "conversations.list"
+    )
+    if channels['ok']:
+        for channel in channels['channels']:
+            if channel['is_member']:
+                channelid = channel['id']
+                slack_client.api_call(
+                    "chat.postMessage",
+                    channel=channelid,
+                    text="hello"
+                )
+    else:
+        # TODO: erreur à afficher
+        pass
