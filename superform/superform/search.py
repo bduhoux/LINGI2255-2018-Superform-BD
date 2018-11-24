b0c90953188f93d8905c6134bf3843ab8f7d6252
bensim602 <benjamin.simon@student.uclouvain.be>
2018-11-24 18:20:22 +0100

    [Search] add option posts and modify tests to hold change

--- superform/superform/search.py
+++ superform/superform/search.py
@@ -16,6 +16,8 @@ def search():
     if request.method == 'GET':
         return render_template('search.html', l_chan=l_chan, post=False)
     else:
+        user = User.query.get(user_id) if session.get("logged_in", False) else None
+        print(user)
         pattern = request.form.get('search_word')
         chan = request.form.getlist('search_chan')
         status = request.form.getlist('post_status')
@@ -23,12 +25,18 @@ def search():
         date_from = request.form.get('date_from')
         date_until = request.form.get('date_until')
         search_type = request.form.get('search_type') == 'keyword'
-        filter_parameter = make_filter_parameter(user_id,pattern,chan,status,loc,date_from,date_until,search_type)
+        search_table = request.form.get('search_table')
+        print(search_table)
+        filter_parameter = make_filter_parameter(user_id, pattern, chan, status, loc, date_from, date_until,
+                                                 search_type, search_table)
         search_result = query_maker(filter_parameter)
-        return render_template('search.html', l_chan=l_chan, publishing=search_result, post=True)
+        return render_template('search.html', l_chan=l_chan, publishing=search_result, post=True,
+                               moderate_channel=[x.id for x in get_moderate_channels_for_user(user)],
+                               search_table=search_table, user_id=user.id)
 
 
-def make_filter_parameter(user_id,pattern,channels,post_status,search_location,date_from=False,date_until=False,search_by_keyword=False):
+def make_filter_parameter(user_id, pattern, channels, post_status, search_location, date_from=False, date_until=False,
+                          search_by_keyword=False, search_table="Publishing"):
     user = User.query.get(user_id) if session.get("logged_in", False) else None
     return {
         'user': user,
@@ -40,6 +48,7 @@ def make_filter_parameter(user_id,pattern,channels,post_status,search_location,d
         'search_by_keyword': search_by_keyword,
         'date_from': date_from,
         'date_until': date_until,
+        'search_table': Post if search_table == "Post" else Publishing
     }
 
 
@@ -50,7 +59,8 @@ def query_maker(filter_parameter):
     :param filter_parameter: A dictionary containing the different filter parameters
     :return: The wanted query
     """
-    return db.session.query(Publishing).filter(filters(filter_parameter)).all()
+    #print(db.session.query(filter_parameter["search_table"]).filter(filters(filter_parameter)))
+    return db.session.query(filter_parameter["search_table"]).filter(filters(filter_parameter)).all()
 
 
 def filters(filter_parameter):
@@ -61,63 +71,74 @@ def filters(filter_parameter):
     :param filter_parameter: a dictionary representing the different filter to apply
     :return: the list of publication according to the filter para#dmeter
     """
-    return filter_query_accessible_publications(filter_parameter["user"]) & filter_query_channel(
-        filter_parameter["channels"]) & filter_query_status(filter_parameter["states"]) & filter_query_title_content(
-        filter_parameter.get("search_in_title", None), filter_parameter.get("search_in_content", None),
-        filter_parameter.get("searched_words", None), filter_parameter.get("search_by_keyword", None)) & filter_date(
-        filter_parameter.get("date_from", None), filter_parameter.get("date_until", None))
+    return filter_query_accessible_publications(filter_parameter["user"], filter_parameter["search_table"]) \
+                       & filter_query_channel(filter_parameter["channels"], filter_parameter["search_table"]) \
+                       & filter_query_status(filter_parameter["states"], filter_parameter["search_table"]) \
+                       & filter_query_title_content(filter_parameter.get("search_in_title", None),
+                                                    filter_parameter.get("search_in_content", None),
+                                                    filter_parameter.get("searched_words", None),
+                                                    filter_parameter.get("search_by_keyword", None),
+                                                    filter_parameter["search_table"]) \
+                       & filter_date(filter_parameter.get("date_from", None), filter_parameter.get("date_until", None),
+                             filter_parameter["search_table"])
 
 
-def filter_query_accessible_publications(user):
+def filter_query_accessible_publications(user, table):
     """
     A function which returns the different publication permitted by an user.
 
     :param user: a User object representing the current user.
     :return: the filter parameters allowing user only seeing publishings he has haccess.
     """
-    condition = (Publishing.post_id == None)
-    if not user.admin:
-        user_post = db.session.query(Post).filter(Post.user_id == user.id)
-        for post in user_post:
-            condition = condition | (Publishing.post_id == post.id)
+    condition = (table.post_id == None) if table == Publishing else (table.id == None)
+    user_post = db.session.query(Post).filter(Post.user_id == user.id)
+    for post in user_post:
+        condition = condition | (table.post_id == post.id) if table == Publishing else condition | (
+                table.id == post.id)
+
+    if table == Publishing:
         for chan in get_moderate_channels_for_user(user):
-            condition = condition | (Publishing.channel_id == chan.id)
-    else:
-        condition = (Publishing.post_id != None)
+            condition = condition | (table.channel_id == chan.id)
+
+
     return condition
 
 
-def filter_query_channel(channels):
+def filter_query_channel(channels, table):
     """
         A function which returns the different publications published in a particular channel
 
         :param channels: a list of channel_id
         :return: the filter parameters allowing to show publishings in the channels defined by the input channels
         """
-    condition = (Publishing.post_id == None)
+    condition = (table.post_id == None) if table == Publishing else (table.id == None)
+    if table == Post:
+        return (table.id != None)
     if not channels:
-        return (Publishing.post_id != None)
+        return (table.post_id != None)
     for chan_id in channels:
-        condition = condition | (Publishing.channel_id == chan_id)
+        condition = condition | (table.channel_id == chan_id)
     return condition
 
 
-def filter_query_status(states):
+def filter_query_status(states, table):
     """
     A function which returns a filter allowing only publishings with certain state
 
-    :param states: A list containing the states the publishings has to possess
+    :param states: A list containing the states the tables has to possess
     :return: a filter parameter allowing only publishings with a state defined by states
     """
+    if table == Post:
+        return (table.id != None)
     if not states:
-        return (Publishing.post_id != None)
-    condition = (Publishing.post_id == None)
+        return (table.post_id != None)
+    condition = (table.post_id == None)
     for state in states:
-        condition = condition | (Publishing.state == state)
+        condition = condition | (table.state == state)
     return condition
 
 
-def filter_query_title_content(title, content, searched_words, split_words):
+def filter_query_title_content(title, content, searched_words, split_words,table):
     """
     A function which returns a a filter allowing publishings with a particular content/title
 
@@ -129,28 +150,28 @@ def filter_query_title_content(title, content, searched_words, split_words):
     """
     if split_words:
         if (searched_words == '') | (searched_words == ' ') | (searched_words is None):
-            return (Publishing.post_id != None)
-        condition = (Publishing.post_id == None)
+            return (table.post_id != None) if table == Publishing else (table.id != None)
+        condition = (table.post_id == None)
         for word in searched_words.split():
             if title & content:
-                condition = condition | Publishing.title.contains(word) | Publishing.description.contains(word)
+                condition = condition | table.title.contains(word) | table.description.contains(word)
             elif title:
-                condition = condition | Publishing.title.contains(word)
+                condition = condition | table.title.contains(word)
             else:
-                condition = condition | Publishing.description.contains(word)
+                condition = condition | table.description.contains(word)
         return condition
     elif not split_words:
         if title & content:
-            return Publishing.title.contains(searched_words) | Publishing.description.contains(searched_words)
+            return table.title.contains(searched_words) | table.description.contains(searched_words)
         elif title:
-            return Publishing.title.contains(searched_words)
+            return table.title.contains(searched_words)
         else:
-            return Publishing.description.contains(searched_words)
+            return table.description.contains(searched_words)
     else:
-        return Publishing.post_id != None  # Return true means all publishings are accepted
+        return table.post_id != None  # Return true means all publishings are accepted
 
 
-def filter_date(date_from, date_until):
+def filter_date(date_from, date_until, table):
     """
     Return a filter parameter allowing only Publishing from a particular and/or to a particular date (if none of them
     are expressed, there will be no filtering)
@@ -159,12 +180,11 @@ def filter_date(date_from, date_until):
     :param date_until: if not Node, filter only publishing posted before a particular date
     :return: A binary Expression filtering the requested publishings by date
     """
-    condition = (Publishing.post_id != None)
+    condition = (table.post_id != None) if table == Publishing else (table.id != None)
     if date_from:
         date = datetime.strptime(date_from, '%Y-%m-%d')
-        condition = condition & (Publishing.date_from >= date)
+        condition = condition & (table.date_from >= date)
     if date_until:
         date = datetime.strptime(date_until, '%Y-%m-%d')
-        condition = condition & (Publishing.date_until <= date)
+        condition = condition & (table.date_until <= date)
     return condition
-
