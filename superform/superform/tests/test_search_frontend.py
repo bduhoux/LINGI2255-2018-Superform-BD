bf11660aabceaaced9cc7339ae0f4a8122e57063
bensim602 <benjamin.simon@student.uclouvain.be>
2018-11-26 21:34:21 +0100

    first test selenium working

--- superform/superform/tests/test_search_frontend.py
+++ superform/superform/tests/test_search_frontend.py
@@ -1,20 +1,24 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
 import pytest
 from selenium import webdriver
 import os, tempfile
 from superform import db, create_app
 from flask import url_for
 from urllib.request import urlopen
+from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture
 def app():
     app = create_app(True)
     return app
 
-@pytest.mark.usefixtures('live_server')
+
 class TestLiveServer:
     def test_basic(self):
         driver = webdriver.Firefox()
-        driver.get(url_for('.index', _external=True))
+        driver.get('http://127.0.0.1:5000/')
         driver.find_element_by_link_text("Login").click()
         driver.find_element_by_name("j_username").click()
         driver.find_element_by_name("j_username").clear()
@@ -23,11 +27,13 @@ class TestLiveServer:
         driver.find_element_by_name("j_password").clear()
         driver.find_element_by_name("j_password").send_keys("myself")
         driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+        wait = WebDriverWait(driver, 10)
+        element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Search')))
         driver.find_element_by_link_text("Search").click()
         driver.find_element_by_id("search_word").click()
         driver.find_element_by_id("search_word").clear()
         driver.find_element_by_id("search_word").send_keys("test")
         driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Search'])[2]/following::div[4]").click()
         driver.find_element_by_id("submit_search").click()
-        assert driver.title == 'Index - Superform', driver.title
+        assert driver.title == 'Search - Superform', driver.title
 
