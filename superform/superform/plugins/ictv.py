03934fd192b3bf01cfeac1f4b141fbf84dac7295
ImnotBarbarossa <antoine.denauw@student.uclouvain.be>
2018-11-29 10:02:17 +0100

    First backend part

--- superform/superform/plugins/ictv.py
+++ superform/superform/plugins/ictv.py
@@ -1,11 +1,27 @@
 from flask import current_app
 import json
 
-FIELDS_UNAVAILABLE = ['Title', 'Description']
+FIELDS_UNAVAILABLE = ['Title', 'Description', 'Linkurl', 'Image']
 CONFIG_FIELDS = []
 
 
-def run(publishing, channel_config):
-    # We don't need to do anything here. We'll just query the database when a
-    # user requests the RSS feed.
+def run(publishing):
+
+    # status_list = [y for x, y in json.loads(publishing.extra)['capsules']]
+    # if publishing.image_url is not '':
+    #     if publishing.logo is not '':
+    #         return publish_list(status_list, image=publishing.image_url, logo=publishing.logo)
+    #     else:
+    #         return publish_list(status_list, image=publishing.image_url)
+    # else:
+    #     return publish_list(status_list)
+
     print(publishing)
+
+
+# def publish_list(statuslist, image=None, logo=None):
+#     a = []
+#     for status in statuslist[:-1]:
+#         a.append(status)
+#     a.append(statuslist[len(statuslist) - 1], image, logo)
+#     return a
