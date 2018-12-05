fabd6edc6907dedb52ea1075acc2aa0a52055e0b
Shawcker <antoine.popeler@student.uclouvain.be>
2018-12-05 17:08:28 +0100

    complete modification of the delete feature (no confirmation popup yet)

--- superform/superform/delete.py
+++ superform/superform/delete.py
@@ -1,14 +1,87 @@
 from flask import Blueprint, url_for, redirect, session, render_template, request
 
+import webbrowser
+
 from superform.utils import login_required
-from superform.models import db, Post, Publishing, User
+from superform.models import db, Post, Publishing, User, Channel
+from superform.users import get_moderate_channels_for_user, is_moderator
 
 from superform.plugins.facebook import delete
+
+import json
 
 delete_page = Blueprint('delete', __name__)
 
 
-@delete_page.route('/delete/<int:id>', methods=['GET', 'POST'])
+@delete_page.route('/delete/<int:id>', methods=['GET'])
+@login_required()
+def delete(id):
+    user = User.query.get(session.get("user_id", "")) if session.get("logged_in", False) else None
+    drafts = []
+    unmoderated = []
+    posted = []
+
+    has_draft = False
+    has_unmoderated = False
+    has_posted = False
+
+    is_facebook = False
+    is_wiki = False
+
+    if user is not None:
+        setattr(user, 'is_mod', is_moderator(user))
+        post = db.session.query(Post).filter_by(id=id).first()
+        if post is not None:
+            post_user_id = post.user_id
+            if post_user_id == user.id:
+                publishings = db.session.query(Publishing).filter(Publishing.post_id == id).all()
+                for pub in publishings:
+                    # The publishing is a draft
+                    if pub.state == -1:
+                        drafts.append(pub)
+                        has_draft = True
+
+                    # The publishing has been submitted for review
+                    elif pub.state == 0:
+                        unmoderated.append(pub)
+                        has_unmoderated = True
+
+                    # The publishing has been posted
+                    elif pub.state == 1:
+                        posted.append(pub)
+                        has_posted = True
+
+                        channel = db.session.query(Channel).filter(Channel.id == pub.channel_id).first()
+                        # The channel is Facebook
+                        if channel == "superform.plugins.facebook":
+                            is_facebook = True
+                        # The channel is Wiki
+                        elif channel == "superform.plugins.wiki":
+                            is_wiki = True
+
+                    # The publishing has been archived
+                    else:
+                        pass
+            else:
+                # The user trying to delete the post is not the one who created it
+                return render_template('403.html'), 403
+        else:
+            # The post does not exist
+            return render_template('404.html'), 404
+    else:
+        # User is not connected
+        return render_template('403.html'), 403
+
+    if has_draft or has_unmoderated or has_posted:
+        return render_template("delete.html", user=user, post=post, draft_pubs=drafts,
+                               unmoderated_pubs=unmoderated, posted_pubs=posted, is_facebook=is_facebook,
+                               is_wiki=is_wiki, has_draft=has_draft, has_unmoderated=has_unmoderated,
+                               has_posted=has_posted)
+    else:
+        return delete_post(id)
+
+
+@delete_page.route('/delete_post/<int:id>', methods=['GET', 'POST'])
 @login_required()
 def delete_post(id):
     user = User.query.get(session.get("user_id", "")) if session.get("logged_in", False) else None
@@ -20,27 +93,17 @@ def delete_post(id):
             if post_user_id == user.id:
                 publishings = db.session.query(Publishing).filter(Publishing.post_id == id).all()
                 post_del_cond = True
-                for pub in publishings:
-                    # If the publishing is not yet validated
-                    if is_not_validated(pub):
-                        db.session.delete(pub)
-                        db.session.commit()
-                    # If the publishing has been posted already (not archived)
-                    elif pub.state == 1:
-                        # If the checkbox for Facebook is checked
-                        if request.form.get('Facebook'):
-                            # Delete post on Facebook
-                            #fb_post_id = pub.extra
-                            # delete(fb_post_id)
-                            pass
-                    else:
-                        post_del_cond = False
+                for _ in publishings:
+                    # If there is any publishing
+                    post_del_cond = False
 
-                # post = db.session.query(Post).filter(Post.id == id).first()
                 # If every publishing linked to the post has been deleted (otherwise violation of constraint in db)
                 if post_del_cond:
                     db.session.delete(post)
                     db.session.commit()
+                else:
+                    # TODO: notify the user about remaining publishings (using flash msg)
+                    pass
             else:
                 # The user trying to delete the post is not the one who created it
                 return render_template('403.html'), 403
@@ -54,8 +117,43 @@ def delete_post(id):
     return redirect(url_for('index'))
 
 
-def is_not_validated(pub):
-    if pub.state != -1 and pub.state != 0:
-        return False
+@delete_page.route('/delete_publishing/<int:post_id>/<int:channel_id>', methods=['GET', 'POST'])
+@login_required()
+def delete_publishing(post_id, channel_id):
+    user = User.query.get(session.get("user_id", "")) if session.get("logged_in", False) else None
 
-    return True
+    if user is not None:
+        post = db.session.query(Post).filter_by(id=post_id).first()
+        if post is not None:
+            post_user_id = post.user_id
+            if post_user_id == user.id:
+                publishings = db.session.query(Publishing).filter(Publishing.post_id == post_id).all()
+                channel = db.session.query(Channel).filter(Channel.id == channel_id).first()
+                for pub in publishings:
+                    if pub.channel_id == channel.id:
+                        # The publishing has been posted
+                        if pub.state == 1:
+                            # It is posted on Facebook
+                            if channel.module == "superform.plugins.facebook":
+                                # TODO: check if user is connected
+                                extra = json.loads(pub.extra)
+                                delete(extra["facebook_post_id"])
+
+                            # It is posted on Wiki
+                            elif channel.module == "superform.plugins.wiki":
+                                pass
+
+                        db.session.delete(pub)
+                        db.session.commit()
+
+            else:
+                # The user trying to delete the post is not the one who created it
+                return render_template('403.html'), 403
+        else:
+            # The post does not exist
+            return render_template('404.html'), 404
+    else:
+        # User is not connected
+        return render_template('403.html'), 403
+
+    return redirect(url_for('delete.delete', id=post_id))
