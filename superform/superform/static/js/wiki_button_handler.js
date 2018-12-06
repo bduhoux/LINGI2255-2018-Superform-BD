4503cf7ace560aab8e623bd0e0b331ba3ba259e5
ElNiak <christophe.crochet@student.uclouvain.be>
2018-12-06 12:27:06 +0100

    - V1 Wiki +  Test

--- superform/superform/static/js/wiki_button_handler.js
+++ superform/superform/static/js/wiki_button_handler.js
@@ -56,24 +56,24 @@ function interpreter(str) {
     res = replace_link1(res);
     res = replace_link2(res);
 
-  res = res.replace(/'''/g,"");
-  res = res.replace(/''/g," ");
-  res = replace_big1(res);
-  res = replace_big2(res);
-  res = replace_small1(res);
-  res = replace_small2(res);
-  res = replace_sub1(res);
-  res = replace_sub2(res);
-  res = replace_exp1(res);
-  res = replace_exp2(res);
-  res = res.replace(/!!/g," ");
+    res = res.replace(/'''/g,"");
+    res = res.replace(/''/g," ");
+    res = replace_big1(res);
+    res = replace_big2(res);
+    res = replace_small1(res);
+    res = replace_small2(res);
+    res = replace_sub1(res);
+    res = replace_sub2(res);
+    res = replace_exp1(res);
+    res = replace_exp2(res);
+    res = res.replace(/!!/g," ");
 
-  return res;
+    return res;
 }
 
 function replace_link1(str){
-  let str2 = "<a class=\"urllink\">";
-  return str.replace(/\[\[/g,str2);
+    let str2 = "<a class=\"urllink\">";
+    return str.replace(/\[\[/g,str2);
 }
 
 function replace_link2(str){
@@ -126,72 +126,72 @@ function replace_heading(str) {
 }
 
 function insButton(mopen, mclose, mtext, mlabel, mkey) {
-  console.log("insButton");
-  if (mkey > '') { mkey = 'accesskey="' + mkey + '" ' }
-  document.write("<a tabindex='-1' " + mkey + "onclick=\"insMarkup('"
-    + mopen + "','"
-    + mclose + "','"
-    + mtext + "');\">"
-    + mlabel + "</a>");
+    console.log("insButton");
+    if (mkey > '') { mkey = 'accesskey="' + mkey + '" ' }
+    document.write("<a tabindex='-1' " + mkey + "onclick=\"insMarkup('"
+        + mopen + "','"
+        + mclose + "','"
+        + mtext + "');\">"
+        + mlabel + "</a>");
 }
 
 function insMarkup() {
-  var func = false, tid='wiki_descriptionpost', mopen = '', mclose = '', mtext = '';
-  if(typeof arguments[0] == 'function') {
-    var func = arguments[0];
-    if(arguments.length > 1) tid = arguments[1];
-    mtext = func('');
-  }
-  else if (arguments.length >= 3) {
-    var mopen = arguments[0], mclose = arguments[1], mtext = arguments[2];
-    if(arguments.length > 3) {
-        tid = arguments[3];
-        tid = tid.id;
+    var func = false, tid='wiki_descriptionpost', mopen = '', mclose = '', mtext = '';
+    if(typeof arguments[0] == 'function') {
+        var func = arguments[0];
+        if(arguments.length > 1) tid = arguments[1];
+        mtext = func('');
     }
-  }
+    else if (arguments.length >= 3) {
+        var mopen = arguments[0], mclose = arguments[1], mtext = arguments[2];
+        if(arguments.length > 3) {
+            tid = arguments[3];
+            tid = tid.id;
+        }
+    }
 
-  var tarea = document.getElementById(tid);
+    var tarea = document.getElementById(tid);
 
-  if (tarea.setSelectionRange > '') {
-    var p0 = tarea.selectionStart;
-    var p1 = tarea.selectionEnd;
-    var top = tarea.scrollTop;
-    var str = mtext;
-    var cur0 = p0 + mopen.length;
-    var cur1 = p0 + mopen.length + str.length;
-    while (p1 > p0 && tarea.value.substring(p1-1, p1) == ' ') p1--; 
-    if (p1 > p0) {
-      str = tarea.value.substring(p0, p1);
-      if(func) str = func(str);
-      cur0 = p0 + mopen.length + str.length + mclose.length;
-      cur1 = cur0;
-    }
-    tarea.value = tarea.value.substring(0,p0)
-      + mopen + str + mclose
-      + tarea.value.substring(p1);
-    tarea.focus();
-    tarea.selectionStart = cur0;
-    tarea.selectionEnd = cur1;
-    tarea.scrollTop = top;
-  } else if (document.selection) {
-    var str = document.selection.createRange().text;
-    tarea.focus();
-    let range = document.selection.createRange();
-    if (str == '') {
-      range.text = mopen + mtext + mclose;
-      range.moveStart('character', -mclose.length - mtext.length );
-      range.moveEnd('character', -mclose.length );
-    } else {
-      if (str.charAt(str.length - 1) == " ") {
-        mclose = mclose + " ";
-        str = str.substr(0, str.length - 1);
-        if(func) str = func(str);
-      }
-      range.text = mopen + str + mclose;
-    }
-    range.select();
-  } else { tarea.value += mopen + mtext + mclose; }
-  return;
+    if (tarea.setSelectionRange > '') {
+        var p0 = tarea.selectionStart;
+        var p1 = tarea.selectionEnd;
+        var top = tarea.scrollTop;
+        var str = mtext;
+        var cur0 = p0 + mopen.length;
+        var cur1 = p0 + mopen.length + str.length;
+        while (p1 > p0 && tarea.value.substring(p1-1, p1) == ' ') p1--;
+        if (p1 > p0) {
+            str = tarea.value.substring(p0, p1);
+            if(func) str = func(str);
+            cur0 = p0 + mopen.length + str.length + mclose.length;
+            cur1 = cur0;
+        }
+        tarea.value = tarea.value.substring(0,p0)
+            + mopen + str + mclose
+            + tarea.value.substring(p1);
+        tarea.focus();
+        tarea.selectionStart = cur0;
+        tarea.selectionEnd = cur1;
+        tarea.scrollTop = top;
+    } else if (document.selection) {
+        var str = document.selection.createRange().text;
+        tarea.focus();
+        let range = document.selection.createRange();
+        if (str == '') {
+            range.text = mopen + mtext + mclose;
+            range.moveStart('character', -mclose.length - mtext.length );
+            range.moveEnd('character', -mclose.length );
+        } else {
+            if (str.charAt(str.length - 1) == " ") {
+                mclose = mclose + " ";
+                str = str.substr(0, str.length - 1);
+                if(func) str = func(str);
+            }
+            range.text = mopen + str + mclose;
+        }
+        range.select();
+    } else { tarea.value += mopen + mtext + mclose; }
+    return;
 }
 
 
